62b42e84e5a36c1941eb2e71e692eafaffcf92b7
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2022-10-14 08:14:03 -0500

    Add matches with their status

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,10 +1,10 @@
-import { matchesFixtures, lastTenGames } from '../api/matchesFixtures';
+import { matchesFixtures, lastTenGames, roundsLeague } from '../api/matchesFixtures';
 import { useEffect, useState } from 'react';
 
 export function Matches() {
 
     const [fixtures, setFixtures] = useState([]);
-    const [openList, setOpenList] = useState(false);
+    const [openList, setOpenList] = useState({isToggleOn: false});
     const [recentTenGames, setRecentTenGames] = useState([]);
     
     useEffect(() => {
@@ -19,26 +19,80 @@ export function Matches() {
             .catch((err) => console.log("error", err));
     }, []);
 
-
     const toggleList = () => {
-        setOpenList(prevState => !prevState);
-    };
+        setOpenList(prevState => ({ 
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    function convertDate(item) {
+        const theDate = new Date(Date.parse(item));
+        const finalDate = theDate.toLocaleString();
+        return finalDate
+    }
+
+    function statusMatchLong(item) {
+        if (item === "Not Started") {
+            return "Sin iniciar"
+        }else if (item === "In Progress") {
+            return "En vivo"
+        }else if (item === " First Half, Kick Off"){
+            return "Primer saque"
+        }else if (item === "Second Half, 2nd Half Started") {
+            return "Segundo tiempo"
+        }else if (item === "Match Finished") {
+            return "Finalizado"
+        }else if (item === "Match Postponed") {
+            return "Pospuesto"
+        }else if (item === "First Half") {
+            return "Medio tiempo"
+        }else if (item === "Second Half") {
+            return "Tiempo completo"
+        }else if (item === "Extra Time") {
+            return "Tiempo extra"
+        }else if (item === "Penalty In Progress") {
+            return "Penales"
+        }else if (item === "Match Finished After Extra Time") {
+            return "Finalizado extra"
+        }else if (item === "Match Finished After Penalty") {
+            return "Penales finalizados "
+        }else if (item === "Break Time") {
+            return "Descanso"
+        }else if (item === "Match Suspended") {
+            return "Suspendido"
+        }else if (item === "Match Interrupted") {
+            return "Interrumpido"
+        }else if (item === "Match Cancelled") {
+            return "Cancelado"
+        }else if (item === "Match Abandoned") {
+            return "Abandonado"
+        }else if (item === "Technical Loss") {
+            return "Perdida tecnica"
+        }else if (item === " First Half, Kick Off"){
+            return "Primer saque"
+        }else if (item === "Second Half, 2nd Half Started") {
+            return "Segundo tiempo"
+        }
+    }
     
     return(
         <div className="ContainerMatches">
             <div className="DivTitleMatchesCom">
                 <h1 className="TitleMatches">Calendario de partidos temporada 2022-23</h1>
             </div>
-            {openList ? (
+            {openList.isToggleOn ? (
                 <div className="DivTableMatches">
                     {fixtures.map((matches) => ( 
+                        
                         <table key={matches.fixture?.id} className="TableMatches">
                             <tbody  className="TbodyMatches">
                                 <tr className="TrMatches">
                                     <td className="TdMatches">
                                         <div className="DivMatches">
-                                            <div className="DataMatch">{matches.fixture?.date}</div>
-
+                                            <div className="DataMatch">
+                                                {convertDate(matches.fixture?.date)}
+                                            </div>
+                                            <div className="DivRounds">Jornada {matches.league?.round.split("-").slice(1, 2).toString()}</div>
                                             <div className="TeamHome">
                                                 <div className='DivHomeLogo'>
                                                     <img src={matches.teams?.home?.logo} className='TeamHomeLogo'/>
@@ -47,7 +101,14 @@ export function Matches() {
                                                     <h3>{matches.teams?.home?.name}</h3>
                                                 </div>
                                             </div>
-                                                <div className="StatusMatch">vs</div>
+                                                <div className="StatusMatch">
+                                                    <div className='StatusLong'>{statusMatchLong(matches.fixture?.status?.long)}</div>
+                                                    <div className='StatusElapsed'>
+                                                        {matches.fixture?.status?.elapsed}
+                                                    </div>
+                                                    <div className='StatusGoalsHome'>{matches.goals?.home}</div>
+                                                    <div className='StatusGoalsAway'>{matches.goals?.away}</div>
+                                                </div>
                                             <div className="TeamAway">
                                                 <div className='DivAwayLogo'>
                                                     <img src={matches.teams?.away?.logo} className='TeamAwayLogo'/>
@@ -60,7 +121,8 @@ export function Matches() {
                                     </td>
                                 </tr>
                             </tbody>
-                        </table>    
+                        </table>   
+                        
                     ))}  
                 </div>
             ) : <div className="DivTableMatches">
@@ -70,8 +132,10 @@ export function Matches() {
                                 <tr className="TrMatches">
                                     <td className="TdMatches">
                                         <div className="DivMatches">
-                                            <div className="DataMatch">{matches.fixture?.date}</div>
-
+                                            <div className="DataMatch">
+                                                {convertDate(matches.fixture?.date)}
+                                            </div>
+                                            <div className="DivRounds">Jornada {matches.league?.round.split("-").slice(1, 2).toString()}</div>
                                             <div className="TeamHome">
                                                 <div className='DivHomeLogo'>
                                                     <img src={matches.teams?.home?.logo} className='TeamHomeLogo'/>
@@ -80,7 +144,14 @@ export function Matches() {
                                                     <h3>{matches.teams?.home?.name}</h3>
                                                 </div>
                                             </div>
-                                                <div className="StatusMatch">vs</div>
+                                                <div className="StatusMatch">
+                                                    <div className='StatusLong'>{statusMatchLong(matches.fixture?.status?.long)}</div>
+                                                    <div className='StatusElapsed'>
+                                                        {matches.fixture?.status?.elapsed}
+                                                    </div>
+                                                    <div className='StatusGoalsHome'>{matches.goals?.home}</div>
+                                                    <div className='StatusGoalsAway'>{matches.goals?.away}</div>
+                                                </div>
                                             <div className="TeamAway">
                                                 <div className='DivAwayLogo'>
                                                     <img src={matches.teams?.away?.logo} className='TeamAwayLogo'/>
@@ -97,7 +168,12 @@ export function Matches() {
                     ))}  
                 </div> 
             }
-            <div className="DivButtonGames"><button onClick={toggleList} className="GamesButton">Ver mas</button></div>
+
+            <div className="DivButtonGames">
+                <button onClick={() => toggleList()} className="GamesButton">
+                    {openList.isToggleOn ? 'Ver menos' : 'Ver mas'}
+                </button>
+            </div>
         </div>
     );
 }
