5ddee8ceaae42d0fa6a7de93f94631314ed92a99
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-01-25 11:15:06 -0500

    I add style for <li>

--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -24,32 +24,32 @@ const PlayerDetails = ({idPlay, handleError}) => {
         <div className='DivDetailsPlayer'>
             <div className='DivPhotoPlayer'><img className='OnePhotoPlayer' src={playerDetails[0]?.player?.photo} /></div>
             <ul className='FeaturesPlayerUl'>
-                <li className='PlayersTeamLi'><b>Nombre:</b> {playerDetails[0]?.player?.firstname}</li>
-                <li className='PlayersTeamLi'><b>Apellido:</b> {playerDetails[0]?.player?.lastname}</li>
-                <li className='PlayersTeamLi'><b>Edad:</b> {playerDetails[0]?.player?.age} años</li>
-                <li className='PlayersTeamLi'><b>Altura:</b> {playerDetails[0]?.player?.height}</li>
-                <li className='PlayersTeamLi'><b>Peso:</b> {playerDetails[0]?.player?.weight}</li>
-                <li className='PlayersTeamLi'><b>Nacionalidad:</b> {playerDetails[0]?.player?.nationality}</li>
-                <li className='PlayersTeamLi'><b>Numero:</b> {idPlayNumber[0]?.players[0]?.number}</li>
+                <li className='PlayersFeaturesLi'><b>Nombre:</b> {playerDetails[0]?.player?.firstname}</li>
+                <li className='PlayersFeaturesLi'><b>Apellido:</b> {playerDetails[0]?.player?.lastname}</li>
+                <li className='PlayersFeaturesLi'><b>Edad:</b> {playerDetails[0]?.player?.age} años</li>
+                <li className='PlayersFeaturesLi'><b>Altura:</b> {playerDetails[0]?.player?.height}</li>
+                <li className='PlayersFeaturesLi'><b>Peso:</b> {playerDetails[0]?.player?.weight}</li>
+                <li className='PlayersFeaturesLi'><b>Nacionalidad:</b> {playerDetails[0]?.player?.nationality}</li>
+                <li className='PlayersFeaturesLi'><b>Numero:</b> {idPlayNumber[0]?.players[0]?.number}</li>
             </ul>
         </div>
         <div className='DivDetailsPlayerTwo'>
             {playerDetails[0]?.statistics[0]?.games?.position === "Goalkeeper" &&
                 <ul className='FeaturesPlayerUlTwo'>
-                    <li className='PlayersTeamLi'><b>Posición:</b> {playerDetails[0]?.statistics[0]?.games?.position}</li>
-                    <li className='PlayersTeamLi'><b>Apariciones:</b> {playerDetails[0]?.statistics[0]?.games?.appearences}</li>
-                    <li className='PlayersTeamLi'><b>Minutos:</b> {playerDetails[0]?.statistics[0]?.games?.minutes}</li>
-                    <li className='PlayersTeamLi'><b>Precision de pases:</b> {playerDetails[0]?.statistics[0]?.passes?.accuracy}%</li>
-                    <li className='PlayersTeamLi'><b>Asistencias:</b> {playerDetails[0]?.statistics[0]?.goals?.assists === null ? "0" : playerDetails[0]?.statistics[0]?.goals?.assists}</li>
-                    <li className='PlayersTeamLi'><b>Goles recibidos:</b> {playerDetails[0]?.statistics[0]?.goals?.conceded}</li>
-                    <li className='PlayersTeamLi'><b>Remates atajados:</b> {playerDetails[0]?.statistics[0]?.goals?.saves}</li>
-                    <li className='PlayersTeamLi'><b>Duelos ganados:</b> {playerDetails[0]?.statistics[0]?.duels?.won}</li>
-                    <li className='PlayersTeamLi'><b>Faltas cometidas:</b> {playerDetails[0]?.statistics[0]?.fouls?.committed === null ? "0" : playerDetails[0]?.statistics[0]?.fouls?.committed}</li>
-                    <li className='PlayersTeamLi'><b>Penaltis comprometidos:</b> {playerDetails[0]?.statistics[0]?.penalty?.commited === null ? "0" : playerDetails[0]?.statistics[0]?.penalty?.commited}</li>
-                    <li className='PlayersTeamLi'><b>Penaltis perdidos:</b> {playerDetails[0]?.statistics[0]?.penalty?.missed}</li>
-                    <li className='PlayersTeamLi'><b>Penaltis atajados:</b> {playerDetails[0]?.statistics[0]?.penalty?.saved}</li>
-                    <li className='PlayersTeamLi'><b>Tarjetas amarillas:</b> {playerDetails[0]?.statistics[0]?.cards?.yellow}</li>
-                    <li className='PlayersTeamLi'><b>Tarjetas rojas:</b> {playerDetails[0]?.statistics[0]?.cards?.red}</li>
+                    <li className='PlayersFeaturesLi'><b>Posición:</b> {playerDetails[0]?.statistics[0]?.games?.position}</li>
+                    <li className='PlayersFeaturesLi'><b>Apariciones:</b> {playerDetails[0]?.statistics[0]?.games?.appearences}</li>
+                    <li className='PlayersFeaturesLi'><b>Minutos:</b> {playerDetails[0]?.statistics[0]?.games?.minutes}</li>
+                    <li className='PlayersFeaturesLi'><b>Precision de pases:</b> {playerDetails[0]?.statistics[0]?.passes?.accuracy}%</li>
+                    <li className='PlayersFeaturesLi'><b>Asistencias:</b> {playerDetails[0]?.statistics[0]?.goals?.assists === null ? "0" : playerDetails[0]?.statistics[0]?.goals?.assists}</li>
+                    <li className='PlayersFeaturesLi'><b>Goles recibidos:</b> {playerDetails[0]?.statistics[0]?.goals?.conceded}</li>
+                    <li className='PlayersFeaturesLi'><b>Remates atajados:</b> {playerDetails[0]?.statistics[0]?.goals?.saves}</li>
+                    <li className='PlayersFeaturesLi'><b>Duelos ganados:</b> {playerDetails[0]?.statistics[0]?.duels?.won}</li>
+                    <li className='PlayersFeaturesLi'><b>Faltas cometidas:</b> {playerDetails[0]?.statistics[0]?.fouls?.committed === null ? "0" : playerDetails[0]?.statistics[0]?.fouls?.committed}</li>
+                    <li className='PlayersFeaturesLi'><b>Penaltis comprometidos:</b> {playerDetails[0]?.statistics[0]?.penalty?.commited === null ? "0" : playerDetails[0]?.statistics[0]?.penalty?.commited}</li>
+                    <li className='PlayersFeaturesLi'><b>Penaltis perdidos:</b> {playerDetails[0]?.statistics[0]?.penalty?.missed}</li>
+                    <li className='PlayersFeaturesLi'><b>Penaltis atajados:</b> {playerDetails[0]?.statistics[0]?.penalty?.saved}</li>
+                    <li className='PlayersFeaturesLi'><b>Tarjetas amarillas:</b> {playerDetails[0]?.statistics[0]?.cards?.yellow}</li>
+                    <li className='PlayersFeaturesLi'><b>Tarjetas rojas:</b> {playerDetails[0]?.statistics[0]?.cards?.red}</li>
                 </ul>
             }
             {playerDetails[0]?.statistics[0]?.games?.position === "Defender" &&
